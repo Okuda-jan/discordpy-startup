2ce996400bcbfcfe12814256607f163a5e43b1ec
Okuda-jan <80048033+Okuda-jan@users.noreply.github.com>
2021-03-06 23:52:06 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,32 +1,21 @@
 from discord.ext import commands
 import os
 import traceback
-import discord #discord.pyの読み込み
+#import discord #discord.pyの読み込み
 
 bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
 client = discord.Client()
+token = os.environ['DISCORD_BOT_TOKEN']
 
-@bot.event
+@client.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
 
-@bot.command()
+@client.command()
 async def ping(ctx):
     await ctx.send('pong')
 
-# メッセージ受信時に動作する処理
-@client.event
-async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/neko':
-        await message.channel.send('にゃーん')
- 
-bot.run(token)
 client.run(token)
