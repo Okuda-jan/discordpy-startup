90009e346c181d26c6fba0ae0dc982b1e75d0c92
Okuda-jan <80048033+Okuda-jan@users.noreply.github.com>
2021-03-06 23:47:22 +0900

    Create discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,10 +1,11 @@
 from discord.ext import commands
 import os
 import traceback
+import discord #discord.pyの読み込み
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
-
+client = discord.Client()
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -17,5 +18,15 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
-
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
+ 
 bot.run(token)
+client.run(TOKEN)
