404a783fe5ce0350fd294f48203b97284be00bbd
Okuda-jan <80048033+Okuda-jan@users.noreply.github.com>
2021-03-07 00:03:35 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,7 +2,8 @@ from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
+#bot = commands.Bot(command_prefix='/')
+bot = commands.Bot()
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
@@ -17,5 +18,13 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
+@bot.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
 
 bot.run(token)
